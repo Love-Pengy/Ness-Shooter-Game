97366fb467fe12e5df582fc1cfe4b77427fbc3b6
David Hightower <hightowerdr@vcu.edu>
2023-11-05 12:48:30 -0500

    Test Screen

---
+++ map.py
@@ -0,0 +1,67 @@
+import pygame, sys
+
+class screen:
+    def __init__(self):
+        W = 0 #Water
+        C = 1 #Cliff
+        G = 2 #Grass
+        F = 3 #ForestGrass
+
+        BLUE = (0,0,255)
+        BROWN = (150,75,0)
+        GRASSGREEN = (124,252,0)
+        FORESTGREEN = (0,100,0)
+
+        self.TileColor = {W : pygame.image.load("Sprites LMAO/Pitfall.png"),
+                          C : pygame.image.load("Sprites LMAO/Valley_Wall.png"),
+                          G : pygame.image.load("Sprites LMAO/Grass.png"),
+                          F : pygame.image.load("Sprites LMAO/Forest_Grass.png")
+                        }
+
+        self.map1 = [[C,C,C,C,C,C,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,C,C,C,C,C,C,C,C,C,C,C,C,C],
+                     [C,G,G,G,G,G,G,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,F,F,F,F,F,F,F,F,F,F,F,F,C],
+                     [C,G,G,G,G,G,G,G,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,F,F,F,F,F,F,F,F,F,F,F,F,C],
+                     [C,G,G,G,G,G,G,G,G,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,F,F,F,F,F,F,F,F,F,F,F,F,C],
+                     [C,G,G,G,G,G,G,G,G,G,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,F,F,F,F,F,F,F,F,F,F,F,F,C],
+                     [C,G,G,G,G,G,G,G,G,G,G,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,F,F,F,F,F,F,F,F,F,F,F,F,C],
+                     [C,G,G,G,G,G,G,G,G,G,G,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,C],
+                     [C,G,G,G,G,G,G,G,G,G,G,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,C],
+                     [C,G,G,G,G,G,G,G,G,G,G,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,C],
+                     [C,G,G,G,G,G,G,G,G,G,G,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,C],
+                     [C,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,C],
+                     [C,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,F,F,F,F,F,F,F,F,F,F,F,F,C],
+                     [C,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,F,F,F,F,F,F,F,F,F,F,F,F,F,C],
+                     [C,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,F,F,F,F,F,F,F,F,F,F,F,F,F,C],
+                     [C,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,F,F,F,F,F,F,F,F,F,F,F,F,F,C],
+                     [C,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,F,F,F,F,F,F,F,F,F,F,F,F,F,C],
+                     [C,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,F,F,F,F,F,F,F,F,F,F,F,F,F,C],
+                     [C,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,F,F,F,F,F,F,F,F,F,F,F,F,F,C],
+                     [C,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,F,F,F,F,F,F,F,F,F,F,F,F,F,C],
+                     [C,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,F,F,F,F,F,F,F,F,F,F,F,F,F,C],
+                     [C,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,F,F,F,F,F,F,F,F,F,F,F,F,F,C],
+                     [C,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,F,F,F,F,F,F,F,F,F,F,F,F,F,C],
+                     [C,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,F,F,F,F,F,F,F,F,F,F,F,F,F,C],
+                     [C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C]]
+
+TILESIZE = 40
+MAPWIDTH = 45
+MAPHEIGHT = 24
+TestScreen = screen()
+
+#Create Display
+pygame.init()
+DISPLAY = pygame.display.set_mode((MAPWIDTH*TILESIZE,MAPHEIGHT*TILESIZE))
+
+#User Interface
+while True:
+    for event in pygame.event.get():
+        "Quit"
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    for row in range(MAPHEIGHT): #Rows
+        for col in range(MAPWIDTH): #Columns
+            #pygame.draw.rect(DISPLAY,TestScreen.TileColor[TestScreen.map1[row][col]],(col*TILESIZE,row*TILESIZE,TILESIZE,TILESIZE))
+            DISPLAY.blit(TestScreen.TileColor[TestScreen.map1[row][col]],(col*TILESIZE,row*TILESIZE))
+    #Update Display
+    pygame.display.update()
